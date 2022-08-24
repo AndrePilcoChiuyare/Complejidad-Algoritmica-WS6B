c7b2bb116e00900a9ed76949501a5d8dd15f1e58
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2022-08-24 18:08:11 -0500

    Update Ejercicio1.py

--- Semana 1/Ejercicio1.py
+++ Semana 1/Ejercicio1.py
@@ -1,67 +1,38 @@
 import numpy as np
-import array as arr
 
-print(np.random.rand(5))
-#1
+
 def generar(n):
-    np.random.randint(1,100,n)
-#2
-a = arr.array('i', [1, 2, 3,6,7,8,9])
-
+    return np.random.randint(1,100,n)
+ # O(n)
 def mostrar(t):
     for i in range (0,5):
         print(t[i])
-    
-mostrar(a)
-#3
+ # O(n)
 def reversa(original):
     return [original[len(original)-i] for i in range(1,len(original)+1)]
-    
-original=[12,20,25,65,78,45,12,56]
-print(reversa(original))
+ # O(n)
+def minval(t):
+  min_val=t[0]
+  for i in range (0,len(t)):
+    if t[i] < min_val:
+      min_val=t[i]
 
-#4
-lista=[28,91,94,92,91,13,15]
-min_val=lista[0]
+  return min_val
+ # O(n) 
+def media(t):
+  suma=0
+  for i in range(0, len(t)):    
+   suma = suma + t[i];
 
-for i in range(0,len(lista)):
-    if lista[i] < min_val:
-        min_val=lista[i]
+  return (suma/len(t))
+# O(n)
+def ocurencia(t):
+  s=[]
+  i=0
+  index=len(t)
+  while(index>0):
+      s.append(t.count(i))
+      index=index-1
 
-print(min_val)
-#5 media aritmetica
-arr = [1, 2, 3, 4, 5];     
-sum = 0;    
-     
-#Loop through the array to calculate sum of elements    
-for i in range(0, len(arr)):    
-   sum = sum + arr[i];    
-     
-print(sum/len(arr));  
-
-
-
-
-
-EJERCICIO 4 
-n = int(input("Ingrese un numero: "))
-lista = list(range(2, n+1))
-
-i = 0
-while(lista[i]*lista[i] <= n):
-    # Mientras el cuadrado del elemento actual sea menor que el ultimo elemento
-    for num in lista:
-        if num <= lista[i]:
-            # Cada iteracion del while hace que el for comience desde el primer elemento. 
-            # Esto es para omitir los numeros primos ya encontrados
-            continue
-        elif num % lista[i] == 0:
-            # Si un numero es divisible entre el elemento actual del while
-            # de ser asi, entonces eliminarlo de la lista (esto altera la lista)
-            lista.remove(num)
-        else:
-            # Si no es divisible, entonces omitirlo (no hacer nada)
-            pass
-    i += 1 # Incrementa al siguiente elemento de la lista (que ha sido alterada)
-
-print (lista)
+  return s 
+# O(n^2)
